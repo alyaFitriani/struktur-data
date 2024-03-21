300e0a817692d43c3afbf47fa163ac19f6caad76
alyaFitriani <162670095+alyaFitriani@users.noreply.github.com>
2024-03-21 10:00:09 +0700

    Add files via upload

---
+++ Pertemuan2/typeData/BiodatabocilJava.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package typeData;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class BiodatabocilJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // biodata bocil menggunakan 5 tipe data
+        String nama = "Bocil" ;
+        String nim = "2355201037" ;
+        int umur = 19 ;
+        Double BB = 39.8 ;
+        char goldar = '-' ;
+        boolean perempuan = true ;
+       
+        
+        System.out.println("=================BIODATA===================");
+        System.out.println("NIM          : " + nim);
+        System.out.println("NAMA         : " + nama);
+        System.out.println("UMUR         : " + umur);
+        System.out.println("Berat Badan  : " + BB) ;
+        System.out.println("Goldar       : " + goldar);
+        System.out.println("Gender       : " + perempuan);
+    }
+    
+    
+}
